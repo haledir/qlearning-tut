9e6a08c3d85744412180e6766a37f779ebc06925
Haledir <michael.moellmann@freenet.de>
2019-06-19 23:13:00 +0200

    corrected file 4

--- qlearning4.py
+++ qlearning4.py
@@ -9,6 +9,7 @@ import time
 style.use("ggplot")
 
 SIZE = 10
+
 HM_EPISODES = 25000
 MOVE_PENALTY = 1
 ENEMY_PENALTY = 300
@@ -17,7 +18,7 @@ epsilon = 0.9
 EPS_DECAY = 0.9998
 SHOW_EVERY = 3000
 
-start_q_table = "qtable-1560977894.pickle" # None or filename
+start_q_table = None # None or filename
 
 LEARNING_RATE = 0.1
 DISCOUNT = 0.95
@@ -25,6 +26,7 @@ DISCOUNT = 0.95
 PLAYER_N = 1
 FOOD_N = 2
 ENEMY_N = 3
+
 
 d = {1: (255,175,0),
 	 2: (0, 255, 0),
@@ -35,13 +37,17 @@ class Blob:
 	def __init__(self):
 		self.x = np.random.randint(0, SIZE)
 		self.y = np.random.randint(0, SIZE)
+
 	def __str__(self):
 		return f"{self.x}, {self.y}"
 
 	def __sub__(self, other):
-		return (self.x - other.x, self.y - other.y)
+		return (self.x-other.x, self.y-other.y)
 
 	def action(self, choice):
+		'''
+        Gives us 4 total movement options. (0,1,2,3)
+        '''
 		if choice == 0:
 			self.move(x=1, y=1)
 		elif choice == 1:
@@ -52,47 +58,56 @@ class Blob:
 			self.move(x=1, y=-1)
 
 	def move(self, x=False, y=False):
+
+        # If no value for x, move randomly
 		if not x:
 			self.x += np.random.randint(-1, 2)
 		else:
 			self.x += x
 
+        # If no value for y, move randomly
 		if not y:
 			self.y += np.random.randint(-1, 2)
 		else:
 			self.y += y
 
+
+        # If we are out of bounds, fix!
 		if self.x < 0:
 			self.x = 0
 		elif self.x > SIZE-1:
 			self.x = SIZE-1
-
 		if self.y < 0:
 			self.y = 0
 		elif self.y > SIZE-1:
 			self.y = SIZE-1
 
 
-if start_q_table is None:
+if start_q_table is None:	
+    # initialize the q-table#
 	q_table = {}
 	for x1 in range(-SIZE+1, SIZE):
 		for y1 in range(-SIZE+1, SIZE):
 			for x2 in range(-SIZE+1, SIZE):
 				for y2 in range(-SIZE+1, SIZE):
 					q_table[((x1, y1),(x2, y2))] = [np.random.uniform(-5, 0) for i in range(4)]
+
 else:
 	with open(start_q_table, "rb") as f:
 		q_table = pickle.load(f)
 
+
+
+
 episode_rewards = []
-for episode in range (HM_EPISODES):
+
+for episode in range(HM_EPISODES):
 	player = Blob()
 	food = Blob()
 	enemy = Blob()
-
 	if episode % SHOW_EVERY == 0:
-		print(f"on # {episode}, epsilon: {epsilon}")
-		print(f"{SHOW_EVERY} ep mean {np.mean(episode_rewards[-SHOW_EVERY:])}")
+		print(f"on #{episode}, epsilon: {epsilon}")
+		print(f"{SHOW_EVERY} ep mean: {np.mean(episode_rewards[-SHOW_EVERY:])}")
 		show = True
 	else:
 		show = False
@@ -100,7 +115,9 @@ for episode in range (HM_EPISODES):
 	episode_reward = 0
 	for i in range(200):
 		obs = (player-food, player-enemy)
+
 		if np.random.random() > epsilon:
+
 			action = np.argmax(q_table[obs])
 		else:
 			action = np.random.randint(0, 4)
@@ -119,18 +136,19 @@ for episode in range (HM_EPISODES):
 		else:
 			reward = -MOVE_PENALTY
 
+
 		new_obs = (player-food, player-enemy)
 		max_future_q = np.max(q_table[new_obs])
 		current_q = q_table[obs][action]
 
 		if reward == FOOD_REWARD:
-			new_q == FOOD_REWARD
+			new_q = FOOD_REWARD
 		elif reward == -ENEMY_PENALTY:
-			new_q == -ENEMY_PENALTY
+			new_q = -ENEMY_PENALTY
 		else:
 			new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
 
-		q_table[obs][action] == new_q
+		q_table[obs][action] = new_q
 
 		if show:
 			env = np.zeros((SIZE, SIZE, 3), dtype=np.uint8)
